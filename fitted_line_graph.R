fa06b6bdd8cef30d9cd717f21483b11dae9b0b3c
Robert Beebe <rbeebe@ltu.edu>
2013-05-03 12:36:22 -0700

    Update fitted_line_graph.R
    Updated version; fixes bugs, auto-alpha-blending based on (implicit) fit rank, highlights your (implicitly) best-fit-line.

--- fitted_line_graph.R
+++ fitted_line_graph.R
@@ -1,23 +1,40 @@
-fitted_line_graph <- function(user_data)
+fitted_line_graph <- function(progression)
 {
+  #browser()
+  xydataset <- read.csv('data.csv',header=F)
+  # xydataset <- as.data.frame(cbind(1:6, 1:6))
+  names(xydataset) <- c("x","y")
+  names(progression) <- c("b","m")
+  left_x <- 0  # min(xydataset$x)
+  right_x <- max(xydataset$x) * 1.05
   g <- NULL
-  for(n in 1:nrow(user_data))
+  progression$ranking <- nrow(progression):1
+  for(n in 1:nrow(progression))
   {
-    m <- user_data[n,"m"]
-    b <- user_data[n,"b"]
-    rank <- user_data[n,"rank"]
-      
-    min_x <- 0
-    min_y <- b
-    max_x <- 100
-    max_y <- m*100 + b
+    m <- progression[n,"m"]
+    b <- progression[n,"b"]
+    ranking <- progression[n,"ranking"]
     
-    g <- rbind(g, c(min_x,min_y,rank), c(max_x,max_y, rank))
+    left_y <- m*left_x + b
+    right_y <- m*right_x + b
+    
+    g <- rbind(g, c(left_x,left_y,ranking), c(right_x,right_y, ranking))
   }
   g <- as.data.frame(g)
-  origin <- as.data.frame(cbind(1:100, 1:100))
-  names(origin) <- c("x","y")
-  names(g)      <- c("x","y","rank")
-  m <- ggplot(g, aes(x=x, y=y, alpha=-rank, group=rank)) + geom_line(aes(colour=-rank)) + geom_point(data=origin, aes(x=x, y=y),alpha = 0.8, colour="gray40") + scale_colour_continuous(low="white",high="red")
+  names(g) <- c("x","y","r")
+#   print(head(g))
+#   print(head(xydataset))
+  m <- eval(ggplot(xydataset, aes(x=x,y=y))
+    #ggplot(g, aes(x=x, y=y, alpha=-ranking, group=ranking))
+            + scale_x_continuous(breaks=seq(0,10,1))
+            + geom_line(data=g, aes(x=x, y=y, alpha=exp(-r), group=r), colour="gray50")
+#             + scale_colour_discrete(low="grey20", high="grey80")
+            + geom_point(colour="gray25", size=5)
+            + geom_line(data=g[(nrow(g)-1):nrow(g),], aes(x=x, y=y),colour="#33CCFF",size=1.5, alpha=0.8)
+            + theme(legend.position='none')
+            + theme_bw()
+            + coord_cartesian(ylim = c(0,2.2)))
+  
+  
   return(m)
 }
