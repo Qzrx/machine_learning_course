3d6248c830a840b9250c96c92ca2afe1a644c068
Robert Beebe <rbeebe@ltu.edu>
2013-05-03 12:38:48 -0700

    Updated ui.R
    Uglier, but simpler. More plotting area. Good stuff.

--- ui.R
+++ ui.R
@@ -1,19 +1,23 @@
 library(shiny)
 
-# Define UI for application that plots random distributions 
+# Define UI for application that plots random distributions
 shinyUI(pageWithSidebar(
   
   # Application title
-  headerPanel("Linear approximation display tool"),
+  headerPanel("Visualize your machine learning!"),
   
   # Sidebar with a slider input for number of observations
-  sidebarPanel(
-    fileInput("file","File to upload", multiple=FALSE)
+  conditionalPanel(
+    condition = "file != null",
+ #   sidebarPanel(
+      fileInput("file","File to upload\n", multiple=FALSE)
+  #  )
   ),
   
   # Show a plot of the generated distribution
   mainPanel(
     plotOutput("dispPlot"),
-    tableOutput("fileTable")
+#     tableOutput("fileTable")
+    p("Credit: Robert Beebe")
   )
 ))
