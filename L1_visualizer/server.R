7e2c83a1a55fe93d61ed5fdfa70d626e7ea422da
Beebe <rbeebe@lm-sfo-00870996.corp.ebay.com>
2013-05-06 10:44:42 -0700

    Moving the visualizer to its own directory

---
+++ L1_visualizer/server.R
@@ -0,0 +1,22 @@
+library(shiny)
+library(ggplot2)
+source('fitted_line_graph.R')
+
+# Define server logic required to generate and plot a random distribution
+shinyServer(function(input, output) {
+  
+  output$dispPlot <- renderPlot({
+    if(is.null(input$file)){return()}
+    # read intercept,slope file
+    interceptslope <- read.csv(file=input$file$datapath,header=F)
+    names(interceptslope) <- c("b", "m")
+    interceptslope$rank = nrow(interceptslope):1
+    print(fitted_line_graph(interceptslope))
+  })
+  
+  output$fileTable <- renderTable({
+    if(is.null(input$file)){return()}
+    input$file
+  })
+  
+})
